49c0ba4acbd0d1f7e4a1726f33b8eaf5e2e358fd
ArturAwesome_RedmiBook16 <nasyrovartur151998@gmail.com>
2025-02-25 07:50:01 +0300

    start work with CrankNocolsonMethod1d

--- src/methods/include/CrankNicolsonMethod1D.h
+++ src/methods/include/CrankNicolsonMethod1D.h
@@ -1,6 +1,14 @@
 #ifndef CRANKNICOLSONMETHOD1D_H
 #define CRANKNICOLSONMETHOD1D_H
 
-class CrankNicolsonMethod1D : public ImplicitMethod1D {};
+class CrankNicolsonMethod1D : public ImplicitMethod1D {
+public:
+    CrankNicolsonMethod1D() override;
+    getSolution() override;
+    setMatrix() override;
+    setMatrixT() override;
+
+
+};
 
 #endif
