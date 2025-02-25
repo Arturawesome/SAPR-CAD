a300b6ff2afc9019ed24496d403f54f5f399c3b8
ArturAwesome_RedmiBook16 <nasyrovartur151998@gmail.com>
2025-02-25 06:49:22 +0300

    --

--- src/mesh/LinearMesh1D.cpp
+++ src/mesh/LinearMesh1D.cpp
@@ -1,98 +1,113 @@
 #include "LinearMesh1D.h"
 
-LinearMesh1D::LinearMesh1D(int numNodesX, double size_x, const char xyzt){
+LinearMesh1D::LinearMesh1D(int numNodesX, double size_x, const char xyzt)
+{
     numNodes[xyzt] = numNodesX;
     size[xyzt] = size_x;
     this->xyzt = xyzt;
 }
 
-LinearMesh1D::LinearMesh1D(){
-    //std::cout<<"LinearMesh1D() numNodesX = 10; numNodesY = 10; size_x = 1; size_y = 1\n ";
+LinearMesh1D::LinearMesh1D()
+{
+    // std::cout<<"LinearMesh1D() numNodesX = 10; numNodesY = 10; size_x = 1; size_y = 1\n ";
     numNodes['x'] = 10;
     size['x'] = 1;
     this->xyzt = 'x';
 }
 
-
-int LinearMesh1D:: GetNumNodes(const char xyzt){
-    //std::cout<<"GetNumNodes\n";
+int LinearMesh1D::GetNumNodes(const char xyzt)
+{
+    // std::cout<<"GetNumNodes\n";
     return numNodes[xyzt];
 }
 
-void LinearMesh1D::generateMesh(){
+void LinearMesh1D::generateMesh()
+{
 
-    dx =  size[xyzt] / (numNodes[xyzt] -1);
+    dx = size[xyzt] / (numNodes[xyzt] - 1);
 
-    for(int i = 0; i < numNodes[xyzt]; ++i){
+    for (int i = 0; i < numNodes[xyzt]; ++i)
+    {
         nodesPosition2[xyzt].push_back(i * dx);
         nodesPosition.push_back(i * dx);
 
-        if( i == 0 || i ==  numNodes[xyzt]-1){
-            boundaryNodesId.push_back(i );
-            boundaryNodesId2[xyzt].push_back(i );
+        if (i == 0 || i == numNodes[xyzt] - 1)
+        {
+            boundaryNodesId.push_back(i);
+            boundaryNodesId2[xyzt].push_back(i);
         }
     }
 
-    for(int i = 0; i < numNodes[xyzt]; ++i){
-        if (i < numNodes[xyzt]-1 ){
-                elementsId.push_back({i, i+1});
-                if(i == 0 || i == numNodes[xyzt]-2 ){
-                    boundaryElements.push_back({i, i+1});
-                }
-            } else {
-                elementsId.push_back({-1, -1});
+    for (int i = 0; i < numNodes[xyzt]; ++i)
+    {
+        if (i < numNodes[xyzt] - 1)
+        {
+            elementsId.push_back({i, i + 1});
+            if (i == 0 || i == numNodes[xyzt] - 2)
+            {
+                boundaryElements.push_back({i, i + 1});
             }
         }
+        else
+        {
+            elementsId.push_back({-1, -1});
+        }
+    }
 }
 
-
-
-void LinearMesh1D::printMesh(){
+void LinearMesh1D::printMesh()
+{
     std::cout << "Nodes:\n";
-    for (size_t i = 0; i < nodesPosition.size(); ++i) {
+    for (size_t i = 0; i < nodesPosition.size(); ++i)
+    {
         std::cout << i << ": " << nodesPosition[i] << ", ";
     }
 
     std::cout << "\n Elements:\n";
-    for (const auto& elem : elementsId) {
-        for (int id : elem) {
+    for (const auto &elem : elementsId)
+    {
+        for (int id : elem)
+        {
             std::cout << id << " ";
         }
         std::cout << "\n";
     }
 }
 
-
-void LinearMesh1D::saveMesh(std::string name_file) {
+void LinearMesh1D::saveMesh(std::string name_file)
+{
     std::ofstream outFile(name_file);
-    if (!outFile.is_open()) {
+    if (!outFile.is_open())
+    {
         std::cerr << "Error: File is not open!" << std::endl;
         return;
     }
-    for(int i = 0; i < numNodes[xyzt]; ++i){
-        outFile <<std::setw(5) << nodesPosition[i]<<";      ";
+    for (int i = 0; i < numNodes[xyzt]; ++i)
+    {
+        outFile << std::setw(5) << nodesPosition[i] << ";      ";
     }
 
     outFile << std::endl;
-    for(int i = 0; i < numNodes[xyzt]; ++i) {
-        outFile <<std::setw(3) << elementsId[i][0]<< std::setw(3) << elementsId[i][1]<< ";      ";
+    for (int i = 0; i < numNodes[xyzt]; ++i)
+    {
+        outFile << std::setw(3) << elementsId[i][0] << std::setw(3) << elementsId[i][1] << ";      ";
     }
     outFile << std::endl;
     outFile << std::endl;
 
     outFile.close();
-
 }
 
-
-
-double LinearMesh1D:: GetNodesPosition(const char xyzt, int i) {
+double LinearMesh1D::GetNodesPosition(const char xyzt, int i)
+{
     return nodesPosition2[xyzt][i];
 }
 
-std::vector<double> LinearMesh1D::GetNodesPosition(const char xyzt) {
+std::vector<double> LinearMesh1D::GetNodesPosition(const char xyzt)
+{
     auto it = nodesPosition2.find(xyzt);
-    if (it != nodesPosition2.end()) {
+    if (it != nodesPosition2.end())
+    {
         return it->second; // Возвращаем копию вектора
     }
     throw std::invalid_argument("Invalid coordinate key");
--- src/tasks/HeatTask1D.cpp
+++ src/tasks/HeatTask1D.cpp
@@ -1,149 +1,124 @@
 #include "HeatTask1D.h"
 
-HeatTask1D::HeatTask1D(std::unordered_map<std::string, std::string> param){
-    this->paramtersOfTask_ = param;
+HeatTask1D::HeatTask1D(std::unordered_map<std::string, std::string> param) {
+  this->paramtersOfTask_ = param;
 
-    for(const auto &pair: param){
-        if(pair.first == "isStatic") {
-            isStatic_ = pair.second == "true";
-        } else if (pair.first == "isInternalHeatSource") {
-            isInternalHeatSource = pair.second == "true";
-        } else if (pair.first == "time") {
-            time = std::stod(pair.second);
-        } else if (pair.first == "dt") {
-            dt = std::stod(pair.second);
-        } else if (pair.first == "Xsource") {
-            Xsource = std::stod(pair.second);
-        } else if (pair.first == "Ysource") {
-            Ysource = std::stod(pair.second);
-        } else if (pair.first == "Tleft") {
-            Tleft = std::stod(pair.second);
-        } else if (pair.first == "Tright") {
-            Tright = std::stod(pair.second);
-        } else if (pair.first == "alpha") {
-            alpha = std::stod(pair.second);
-        } else if (pair.first == "N") {
-            N = std::stod(pair.second);
-        } else if (pair.first == "L") {
-            L = std::stod(pair.second);
-        } else if (pair.first == "useCustomMesh") {
-            useCustomMesh = pair.second == "false";
-        } else if (pair.first == "useCustomMethod") {
-            useCustomMethod = pair.second == "false";
-        } else if (pair.first == "Qsource") {
-            Qsource = std::stod(pair.second);
-        }
+  for (const auto &pair : param) {
+    if (pair.first == "isStatic") {
+      isStatic_ = pair.second == "true";
+    } else if (pair.first == "isInternalHeatSource") {
+      isInternalHeatSource = pair.second == "true";
+    } else if (pair.first == "time") {
+      time = std::stod(pair.second);
+    } else if (pair.first == "dt") {
+      dt = std::stod(pair.second);
+    } else if (pair.first == "Xsource") {
+      Xsource = std::stod(pair.second);
+    } else if (pair.first == "Ysource") {
+      Ysource = std::stod(pair.second);
+    } else if (pair.first == "Tleft") {
+      Tleft = std::stod(pair.second);
+    } else if (pair.first == "Tright") {
+      Tright = std::stod(pair.second);
+    } else if (pair.first == "alpha") {
+      alpha = std::stod(pair.second);
+    } else if (pair.first == "N") {
+      N = std::stod(pair.second);
+    } else if (pair.first == "L") {
+      L = std::stod(pair.second);
+    } else if (pair.first == "useCustomMesh") {
+      useCustomMesh = pair.second == "false";
+    } else if (pair.first == "useCustomMethod") {
+      useCustomMethod = pair.second == "false";
+    } else if (pair.first == "Qsource") {
+      Qsource = std::stod(pair.second);
     }
-
+  }
 }
 
+void HeatTask1D::getTaskDescription() {
+  std::cout << "void HeatTask1D::getTaskDescription()\n";
+  std::cout
+      << "You solve the heat equation with following paramters:\n you set: \n";
+  std::cout << "useCustomMesh = " << useCustomMesh << "\n";
 
+  std::cout << "time = " << time << ";  dt = " << dt << "\n";
+  std::cout << "isStatic_ = " << isStatic_ << "\n";
 
-void HeatTask1D::getTaskDescription(){
-    std::cout<<"void HeatTask1D::getTaskDescription()\n";
-    std::cout<<"You solve the heat equation with following paramters:\n you set: \n";
-    std::cout<< "useCustomMesh = "<<useCustomMesh<<"\n";
+  std::cout << "isInternalHeatSource = " << isInternalHeatSource << "\n";
+  std::cout << "Xsource = " << Xsource << ";  Ysource = " << Ysource << "\n";
 
-    std::cout<< "time = "<< time<<";  dt = "<<dt<<"\n";
-    std::cout<< "isStatic_ = " << isStatic_<<"\n";
-
-    std::cout<< "isInternalHeatSource = " << isInternalHeatSource<<"\n";
-    std::cout<< "Xsource = "<< Xsource << ";  Ysource = "<< Ysource<<"\n";
-
-    std::cout<< "Tleft = "<< Tleft<< ";  Tright = "<< Tright<<"\n";
-    std::cout<< "alpha = "<< alpha<<"\n";
-    std::cout<< "N = "<< N << ";  L = "<< L<<"\n";
-
+  std::cout << "Tleft = " << Tleft << ";  Tright = " << Tright << "\n";
+  std::cout << "alpha = " << alpha << "\n";
+  std::cout << "N = " << N << ";  L = " << L << "\n";
 }
 
-void HeatTask1D::solveTask(){
-    std::cout<<"void HeatTask1D::solveTask()\n";
-    if(paramtersOfTask_["meshType"] == "LinearMesh1D"){
-        std::cout<<"L = "<<L<<";  N = "<<N <<"\n\n";
-        mesh_ = std::make_shared<LinearMesh1D>(N, L);
-        mesh_ -> generateMesh();
-        std::cout<<"LinearMesh1D was created\n";
-        mesh_ -> printMesh();
+void HeatTask1D::solveTask() {
+  std::cout << "void HeatTask1D::solveTask()\n";
+  if (paramtersOfTask_["meshType"] == "LinearMesh1D") {
+    std::cout << "L = " << L << ";  N = " << N << "\n\n";
+    mesh_ = std::make_shared<LinearMesh1D>(N, L);
+    mesh_->generateMesh();
+    std::cout << "LinearMesh1D was created\n";
+    mesh_->printMesh();
+  }
+
+  if (paramtersOfTask_["method"] == "FiniteDifferenceMethod1D") {
+    setMatrixAndConditions();
+    method_ = std::make_shared<FiniteDifferenceMethod1D>();
+
+    solution = method_->getSolution(matrixA, matrixB);
+    plotSolution("HeatTask1D_" + paramtersOfTask_["method"] + "_" +
+                 paramtersOfTask_["meshType"]);
+  }
+}
+
+void HeatTask1D::setMatrixAndConditions() {
+  // std::cout<<"void HeatTask1D::setMatrixAndConditions()\n\n";
+  double dx;
+  double coeff;
+  matrixB.resize(N, 0.0);
+  matrixA.resize(N, std::vector<double>(N, 0.0));
+
+  for (int i = 1; i < N - 1; ++i) {
+    dx = mesh_->GetNodesPosition('x', i) - mesh_->GetNodesPosition('x', i - 1);
+    coeff = alpha * dx * dx;
+
+    std::vector<double> v(N, 0);
+
+    matrixA[i][i - 1] = 1.0;
+    matrixA[i][i] = -(2.0 + coeff);
+    matrixA[i][i + 1] = 1.0;
+
+    if (isInternalHeatSource) {
+      if (std::abs(i * dx - Xsource) < dx / 2) {
+        matrixB[i] = -coeff * Tright - Qsource * dx * dx;
+      } else {
+        matrixB[i] = -coeff * Tright;
+      }
+    } else {
+      matrixB[i] = -coeff * Tright;
     }
+  }
 
+  matrixA[0][0] = 1.0;
+  matrixB[0] = Tleft;
+  matrixA[N - 1][N - 1] = -1.0 / dx;
+  matrixA[N - 1][N - 2] = 1.0 / dx;
 
-    if(paramtersOfTask_["method"] == "FiniteDifferenceMethod1D"){
-        setMatrixAndConditions();
-        method_ = std::make_shared<FiniteDifferenceMethod1D>();
-
-        solution = method_ -> getSolution(matrixA, matrixB);
-        plotSolution("HeatTask1D_" + paramtersOfTask_["method"] + "_" + paramtersOfTask_["meshType"] );
-    }
-
+  // for(int i = 0; i < N; ++i){
+  //     for (int j = 0; j < N; ++j){
+  //         std::cout<<matrixA[i][j]<<"  ";
+  //     }
+  //     std::cout<<";  "<<matrixB[i]<<"  \n";
+  // }
 }
 
-void HeatTask1D::setMatrixAndConditions(){
-    //std::cout<<"void HeatTask1D::setMatrixAndConditions()\n\n";
-    double dx;
-    double coeff;
-    matrixB.resize(N, 0.0);
-    matrixA.resize(N, std::vector<double>(N, 0.0));
+void HeatTask1D::plotSolution(std::string nameFig) {
+  // std::cout<<"void HeatTask1D::plotSolution()\n";
+  std::vector<double> x = mesh_->GetNodesPosition('x');
 
-    for(int i = 1 ; i < N-1; ++i){
-        dx = mesh_->GetNodesPosition('x', i) - mesh_->GetNodesPosition('x', i-1);
-        coeff = alpha * dx* dx;
-
-        std::vector<double> v(N, 0);
-
-
-        matrixA[i][i-1] = 1.0;
-        matrixA[i][i] = -(2.0 + coeff);
-        matrixA[i][i+1] = 1.0;
-
-
-        if(isInternalHeatSource) {
-            if(std::abs(i * dx - Xsource) < dx / 2) {
-                matrixB[i] = -coeff * Tright - Qsource * dx * dx;
-            } else {
-                matrixB[i] = -coeff * Tright;
-            }
-        } else {
-            matrixB[i] = -coeff * Tright;
-        }
-    }
-
-    matrixA[0][0] = 1.0;
-    matrixB[0] = Tleft;
-    matrixA[N - 1][N - 1] = -1.0 / dx;
-    matrixA[N - 1][N - 2] = 1.0 / dx;
-
-    // for(int i = 0; i < N; ++i){
-    //     for (int j = 0; j < N; ++j){
-    //         std::cout<<matrixA[i][j]<<"  ";
-    //     }
-    //     std::cout<<";  "<<matrixB[i]<<"  \n";
-    // }
+  plt::plot(x, solution, "o-");
+  plt::show();
+  // plt::clf();
 }
-
-
-void HeatTask1D::plotSolution(std::string nameFig){
-    //std::cout<<"void HeatTask1D::plotSolution()\n";
-    std::vector<double> x =  mesh_ -> GetNodesPosition('x');
-
-
-    plt::plot(x, solution, "o-");
-    plt::show();
-    //plt::clf();
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
